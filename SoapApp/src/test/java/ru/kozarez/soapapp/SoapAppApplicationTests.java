57e9ed8b045c8c5537bec5a65153f1f6e77d689b
asdadsdasd <mkozarez12345@mail.ru>
2024-01-16 10:20:39 +0300

    init commit

---
+++ SoapApp/src/test/java/ru/kozarez/soapapp/SoapAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kozarez.soapapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
